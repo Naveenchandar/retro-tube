b275327322b74ce13a2b66628152a4c478af267d
Naveenchandar <naveenchandece@gmail.com>
2022-04-20 09:42:21 +0530

    video home page changes
    * list of categories
    * home banner
    * navbar

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Categories from './categories';
+import './index.css';
 
 export function Home() {
   return (
-    <h1>Home</h1>
+    <section className='home'>
+      <header className='home_header flex align_center justify_center'>
+        <section className='flex align_center justify_center flex_dcolumn'>
+          <h1 className='text_center home_title'>View your favourite club videos</h1>
+          <button className='btn btn_primary explore_btn'>Explore Videos</button>
+        </section>
+      </header>
+      <div className='home_categories'>
+        <Categories />
+      </div>
+    </section>
   )
 }
