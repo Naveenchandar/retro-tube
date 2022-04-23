ed9ea16c543d1f570fa067d5418eba62ce0ca253
Naveenchandar <naveenchandece@gmail.com>
2022-04-24 01:38:05 +0530

    chip component ui changes

---
+++ src/components/chips/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './index.css';
+
+const chipValues = ['all', 'rm', 'mu', 'ju', 'bm'];
+const chipClassName = 'p-1 chips_item';
+
+function Chips({ activeChip, changeChip}) {
+
+  const loadActiveClassName = (val) => {
+    if (chipValues.includes(activeChip) && activeChip === val) {
+      return `${chipClassName} active_chip`;
+    }
+    return chipClassName;
+  }
+
+  return (
+    <div className='chips p-1'>
+      <ul className='flex'>
+        <li className={loadActiveClassName('all')} onClick={() => changeChip('all')}>All</li>
+        <li className={loadActiveClassName('rm')} onClick={() => changeChip('rm')}>Real Madrid</li>
+        <li className={loadActiveClassName('mu')} onClick={() => changeChip('mu')}>Manchester United</li>
+        <li className={loadActiveClassName('ju')} onClick={() => changeChip('ju')}>Juventus</li>
+        <li className={loadActiveClassName('bm')} onClick={() => changeChip('bm')}>Bayern Munich</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Chips
