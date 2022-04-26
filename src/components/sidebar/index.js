2b5a09e01882f7d845c43544640c5556b2ebd1de
Naveenchandar <naveenchandece@gmail.com>
2022-04-26 23:27:08 +0530

    link to navlink changes

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './index.css';
 
 export function Sidebar() {
+    const linkClassName = 'p-1 sidebar_link';
+    const activeLinkClassName = (isActive) => {
+        if(isActive) {
+            return `active ${linkClassName}`
+        }
+        return linkClassName
+    }
     return (
         <aside className='sidebar'>
             <ul className='mt-1 p-2 sidebar_list'>
-                <li><Link to='/' className='p-1 sidebar_link'>Home</Link></li>
-                <li><Link to='/explore' className='p-1 sidebar_link'>Explore</Link></li>
-                <li><Link to='/' className='p-1 sidebar_link'>Playlists</Link></li>
-                <li><Link to='/' className='p-1 sidebar_link'>Watch later</Link></li>
-                <li><Link to='/' className='p-1 sidebar_link'>Liked videos</Link></li>
-                <li><Link to='/' className='p-1 sidebar_link'>History</Link></li>
+                <li><NavLink to='/' className={({ isActive }) => activeLinkClassName(isActive)}>Home</NavLink></li>
+                <li><NavLink to='/explore' className={({ isActive }) => activeLinkClassName(isActive)}>Explore</NavLink></li>
+                <li><NavLink to='/' className={({ isActive }) => activeLinkClassName(isActive)}>Playlists</NavLink></li>
+                <li><NavLink to='/' className={({ isActive }) => activeLinkClassName(isActive)}>Watch later</NavLink></li>
+                <li><NavLink to='/' className={({ isActive }) => activeLinkClassName(isActive)}>Liked videos</NavLink></li>
+                <li><NavLink to='/' className={({ isActive }) => activeLinkClassName(isActive)}>History</NavLink></li>
             </ul>
         </aside>
     )
