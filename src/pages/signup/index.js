cb4322263292398787a7a7f3a7a33564a37b2868
Naveenchandar <naveenchandece@gmail.com>
2022-04-28 23:29:41 +0530

    auth context file name change

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useDocumentTitle } from '../../hooks/useDocumentTitle';
-import { useAuth } from '../../context/useAuth';
+import { useAuth } from '../../context/auth';
 
 export function SignUp() {
     const [info, setUserInfo] = useState({
