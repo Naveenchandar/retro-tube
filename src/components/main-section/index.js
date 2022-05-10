e87e3337aef90ec925fdff14abe78a1d065ada27
Naveenchandar <naveenchandece@gmail.com>
2022-05-10 20:23:09 +0530

    Update index.js

--- src/components/main-section/index.js
+++ src/components/main-section/index.js
@@ -4,7 +4,7 @@ import { NoVideos } from '../no-videos'
 export function MainSection({ data, type, children }) {
     return (
         <section className='w_100 section_videos'>
-            {data > 0 ? children : <NoVideos type='playlists' />}
+            {data > 0 ? children : <NoVideos type={type} />}
         </section>
     )
 }
