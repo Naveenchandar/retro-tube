507f353f259fcbb69047ae5bccab36ef67d9c695
Naveenchandar <naveenchandece@gmail.com>
2022-05-10 09:55:45 +0530

    main section layout component created

---
+++ src/components/main-section/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NoVideos } from '../no-videos'
+
+export function MainSection({ data, type, children }) {
+    return (
+        <section className='w_100 section_videos'>
+            {data > 0 ? children : <NoVideos type='playlists' />}
+        </section>
+    )
+}
