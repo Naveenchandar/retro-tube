08ed1fc215a07ab964aeeabb7143dbd0053264f7
Naveenchandar <37447901+Naveenchandar@users.noreply.github.com>
2022-07-02 21:05:46 +0530

    app css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { ReactNotifications } from 'react-notifications-component'
 import "./App.css";
 import { Footer } from "./components/footer";
-import NavRoutes from "./components/routes";
+import { NavRoutes } from "./components/routes";
 
 function App() {
   return (
     <>
+      <ReactNotifications />
       <NavRoutes />
       <Footer />
     </>
