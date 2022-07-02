bcf06aac9df9339b9008af7704cea924fb4dc831
Naveenchandar <naveenchandece@gmail.com>
2022-07-02 13:09:36 +0530

    react-notificaton css added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-notifications-component/dist/theme.css'
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
