c394ff86e0f20352209a53fb61851d22911c599b
Naveenchandar <naveenchandece@gmail.com>
2022-07-26 07:08:13 +0530

    components barrel export

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+export { Chips } from './chips';
+export { Footer } from './footer';
+export { MainHeader } from './main-header';
+export { MainSection } from './main-section';
+export { MoreOptions } from './more-options';
+export { Navbar } from './navbar';
+export { NoVideos } from './no-videos';
+export { PlaylistModal } from './playlistmodal';
+export { Playlists } from './playlists';
+export { NavRoutes } from './routes';
+export { SearchInput } from './search';
+export { Sidebar } from './sidebar';
+export { Video } from './video';
+export { Videos } from './videos';
