3c6a176a9f2b0bc9ffb8e5816613583bfa72354f
Naveenchandar <naveenchandece@gmail.com>
2022-07-30 20:40:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { Suspense } from 'react';
 import { ReactNotifications } from 'react-notifications-component'
 import "./App.css";
-import { Footer, NavRoutes, ErrorBoundary } from "components";
+import { Footer, NavRoutes } from "components";
 import { useTheme } from 'hooks/useTheme';
 
 function App() {
   const { theme } = useTheme();
   return (
-    <ErrorBoundary>
-      <Suspense fallback={'Loading...'}>
-        <div data-theme={theme}>
-          <ReactNotifications />
-          <NavRoutes />
-          <Footer />
-        </div>
-      </Suspense>
-    </ErrorBoundary>
+    // <ErrorBoundary>
+    //   <Suspense fallback={'Loading...'}>
+    <div data-theme={theme}>
+      <ReactNotifications />
+      <NavRoutes />
+      <Footer />
+    </div>
+    //   </Suspense>
+    // </ErrorBoundary>
   );
 }
 
