1391d6727afafc7c16e1facccfe9170ee593a39c
Naveenchandar <naveenchandece@gmail.com>
2022-07-30 20:23:56 +0530

    Revert "Update index.js"
    This reverts commit bd42df045ec4801d7d2d7c3910caece97ef7fd5d.

--- src/components/error-boundary/index.js
+++ src/components/error-boundary/index.js
@@ -4,8 +4,7 @@ import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
 import { useNavigate } from 'react-router-dom';
 import './index.css';
 
-function ErrorFallback({ error }) {
-    console.log(':::error::::', error.message);
+function ErrorFallback() {
     const { theme } = useTheme();
     const navigate = useNavigate();
     return (
@@ -22,9 +21,8 @@ function ErrorFallback({ error }) {
 
 export function ErrorBoundary({ children }) {
     const myErrorHandler = (error, info) => {
-        console.error(':::info::::', info);
-        console.error(':::info::::', info.componentStack);
-        console.error(':::error::::', error);
+        console.error('info:', info.componentStack);
+        console.error('error:', error);
     }
     return (
         <ReactErrorBoundary FallbackComponent={ErrorFallback} onError={myErrorHandler}>
