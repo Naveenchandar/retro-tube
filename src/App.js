3e02b6ad68b37d8499988607032603928d77ed98
Naveenchandar <naveenchandece@gmail.com>
2022-07-30 20:54:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import { ReactNotifications } from 'react-notifications-component'
 import "./App.css";
 import { Footer, NavRoutes } from "components";
-import { useTheme } from 'hooks/useTheme';
 
 function App() {
-  const { theme } = useTheme();
   return (
-    // <ErrorBoundary>
-    //   <Suspense fallback={'Loading...'}>
-    <div data-theme={theme}>
-      {/* <ReactNotifications /> */}
-      {/* <NavRoutes /> */}
-      <h1>CRA-CreateReactApp</h1>
-      {/* <Footer /> */}
-    </div>
-    //   </Suspense>
-    // </ErrorBoundary>
+    <>
+      <ReactNotifications />
+      <NavRoutes />
+      <Footer />
+    </>
   );
 }
 
