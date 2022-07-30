bd42df045ec4801d7d2d7c3910caece97ef7fd5d
Naveenchandar <naveenchandece@gmail.com>
2022-07-30 20:18:11 +0530

    Update index.js

--- src/components/error-boundary/index.js
+++ src/components/error-boundary/index.js
@@ -4,7 +4,8 @@ import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
 import { useNavigate } from 'react-router-dom';
 import './index.css';
 
-function ErrorFallback() {
+function ErrorFallback({ error }) {
+    console.log(':::error::::', error.message);
     const { theme } = useTheme();
     const navigate = useNavigate();
     return (
@@ -21,8 +22,9 @@ function ErrorFallback() {
 
 export function ErrorBoundary({ children }) {
     const myErrorHandler = (error, info) => {
-        console.error('info:', info.componentStack);
-        console.error('error:', error);
+        console.error(':::info::::', info);
+        console.error(':::info::::', info.componentStack);
+        console.error(':::error::::', error);
     }
     return (
         <ReactErrorBoundary FallbackComponent={ErrorFallback} onError={myErrorHandler}>
